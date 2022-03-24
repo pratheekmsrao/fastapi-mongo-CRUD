f577b29e5ab2616a06bab5b2e25255a61fd8edc9
pratms <pratheekmsdev@gmail.com>
2022-03-24 22:06:41 +0530

    user schema added

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
+from .schemas.user  import UserInfo
 
 app=FastAPI()
 
 @app.get('/')
 async def root():
     return {"message":"Hello World"}
+
+
+@app.get('/user')
+def user(data:UserInfo):
+    return {"data":data}
+    
---
+++ schemas/user.py
@@ -0,0 +1,24 @@
+from tkinter import N
+from typing import Optional
+from pydantic import BaseModel
+
+class UserLogin(BaseModel):
+    email : str
+    password : str
+
+class UserInfo(BaseModel):
+    first_name:str
+    middle_name:Optional[str]=None
+    last_name:str
+    age:Optional[int]=None
+    email:str
+
+class UserAddress(BaseModel):
+    address_line1:str
+    address_line2:Optional[str]=None
+    city:str
+    state:str
+    country:str
+    zip_code:str
+
+
